abf5bcefb7f2d2cdb31a23bf3b353ff17834cdb4
heyoni <hhheyoni@gmail.com>
2021-01-09 23:14:08 +0900

    (0109) create polls

--- myenvi/bin/mysite/mysite/urls.py
+++ myenvi/bin/mysite/mysite/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
+
+# include 함수가 다른 URLconf를 참조할 수 있도록 도와줌
+# 
 urlpatterns = [
+    path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
