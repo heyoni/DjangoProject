ffe81583b0c10e9a0008fa168c53765c0282c81d
heyoni <hhheyoni@gmail.com>
2021-01-10 14:34:17 +0900

    (0110) new env

--- myenvi/bin/mysite/polls/models.py
+++ myenvi/bin/mysite/polls/models.py
@@ -1,9 +1,8 @@
 from django.db import models
-
-# Create your models here.
-class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_data = models.DateTimeField('data published')
+from django.utils import timezone
+class Question (models.Model):
+    question_text = models.CharField (max_length = 200)
+    pub_date = models.DateTimeField ('date published', default = timezone.now)
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
