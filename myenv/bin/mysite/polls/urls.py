da089c956d82ba1299328077dfd9937143a27795
heyoni <hhheyoni@gmail.com>
2021-01-10 18:47:05 +0900

    (0110) poll page

--- myenv/bin/mysite/polls/urls.py
+++ myenv/bin/mysite/polls/urls.py
@@ -2,6 +2,13 @@ from django.urls import path
 
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
+    # /polls/를 호출했을 때 view에 있는 index 메서드를 호출한다.
     path('', views.index, name='index'),
+
+    # /polls/숫자/ 를 호출했을 때 상세페이지를 보여준다.
+    path('<int:question_id>/',views.detail,name='detail'),
+    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
