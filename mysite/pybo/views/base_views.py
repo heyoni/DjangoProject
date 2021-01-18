c75c40d1635ac246f6dd4f6778d1514781f16997
heyoni <hhheyoni@gmail.com>
2021-01-18 21:57:55 +0900

    (0118) 검색창 추가
    새 질문 업로드 안됨

--- mysite/pybo/views/base_views.py
+++ mysite/pybo/views/base_views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.core.paginator import Paginator
-
+from django.db.models import Q
 
 from ..models import Question
 
@@ -10,16 +10,25 @@ def index(request):
     pybo 목록 출력
     """
     # 입력인자
-    page = request.GET.get('page','1')
+    page = request.GET.get('page' '1')  #페이지
+    kw = request.GET.get('kw',' ') #검색어
 
-    #조회
+    #조회 
     question_list = Question.objects.order_by('-create_date')
+    question_list = Question.objects.order_by('-create_date')
+    if kw:
+        question_list = question_list.filter(
+            Q(subject__icontains=kw) |  # 제목검색
+            Q(content__icontains=kw) |  # 내용검색
+            Q(author__username__icontains=kw) |  # 질문 글쓴이검색
+            Q(answer__author__username__icontains=kw)  # 답변 글쓴이검색
+        ).distinct()
 
     #페이징처리
     paginator = Paginator(question_list, 10)
     page_obj = paginator.get_page(page)
 
-    context = {'question_list':page_obj}
+    context = {'question_list': page_obj, 'page': page, 'kw': kw}  # page와 kw가 추가되었다.
     return render(request, 'pybo/question_list.html', context)
 
 
