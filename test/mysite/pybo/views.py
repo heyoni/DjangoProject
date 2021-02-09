2c3fda00bf1420d065187c88c7fc5c602d988d74
heyoni <hhheyoni@gmail.com>
2021-02-09 21:32:31 +0900

    (0209) navbar

--- test/mysite/pybo/views.py
+++ test/mysite/pybo/views.py
@@ -1,12 +1,11 @@
 
 from django.shortcuts import render, get_object_or_404, redirect
-
+from django.utils import timezone
 from .models import Question
 
 # Create your views here.
 def index(request):
     # pybo 목록 출력
-
     question_list = Question.objects.order_by('-create_date')
     context = {'question_list':question_list}
     return render(request, 'index.html', context)
@@ -25,3 +24,5 @@ def answer_create(request, question_id):
     question.answer_set.create(content=request.POST.get('content'), create_date=timezone.now())
 
     return redirect('pybo:detail', question_id=question.id)
+
+
