e330b69331712bac45a2d3e9e8156fb990ddbb66
heyoni <hhheyoni@gmail.com>
2021-02-16 21:31:33 +0900

    (0216) 메인페이지 작성 + 로그인 기능 작성 중

--- jusik/mypage/config/urls.py
+++ jusik/mypage/config/urls.py
@@ -14,10 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-# from pybo import views
+from django.urls import path, include
+from jusik import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
+    path('common/', include('common.urls')),
+    path('jusik/', include('jusik.urls')),
 ]
