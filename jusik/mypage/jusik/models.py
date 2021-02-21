6e9be8c3bf0738f938a26ace72d475e931978158
heyoni <hhheyoni@gmail.com>
2021-02-22 00:40:51 +0900

    (0221) 리스트 가져오기 성공

--- jusik/mypage/jusik/models.py
+++ jusik/mypage/jusik/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Jusik_list(models.Model):
+    buy_date = models.DateTimeField()
+    stock_name = models.CharField(max_length=200)
+    amount = models.IntegerField()
+    buy_price = models.IntegerField()
+    buy_amount = models.IntegerField()
+    sell_price = models.IntegerField()
+    sell_amount = models.IntegerField()
+    profit = models.IntegerField()
+
+    def __str__(self):
+        return self.stock_name
