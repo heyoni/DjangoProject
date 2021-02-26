ae2c5e69b74d3887e3b8ff1d168978cfdb4157c9
heyoni <hhheyoni@gmail.com>
2021-02-26 23:34:49 +0900

    (0226) 에러 잡기 완료/index 수정 중

--- jusik/mypage/jusik/forms.py
+++ jusik/mypage/jusik/forms.py
@@ -4,11 +4,11 @@ from jusik.models import Jusik_list
 class JusikForm(forms.ModelForm):
     class Meta:
         model = Jusik_list
-        fields = ['stock_name','amount','buy_price','sell_price']
+        fields = ['stock_name','amount','buy_price','sell_price','date']
         labels = {
             'stock_name' : '종목명',
             'amount' : '수량',
-            # 'buy_price' : '매수가격',
+            'buy_price' : '매수가격',
             'sell_price' : '매도가격',
             'date' : '날짜'
         }
