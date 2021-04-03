fdc1c4d20cd8d7a434dad28748fe0c8a4ae5b0ca
heyoni <hhheyoni@gmail.com>
2021-04-04 01:34:51 +0900

    (0403) detail page

--- pinterest/pragmatic/accountapp/views.py
+++ pinterest/pragmatic/accountapp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse, reverse_lazy
 from accountapp.models import HelloWorld
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DetailView
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
@@ -28,3 +28,9 @@ class AccountCreateView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/create.html'
+
+
+class AccountDetailView(DetailView):
+    model = User
+    context_object_name = 'target_user'
+    template_name = 'accountapp/detail.html'
