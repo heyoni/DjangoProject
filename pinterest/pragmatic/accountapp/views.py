69d7b5a88f30876579cb2eff1e478289cc7dd2fc
heyoni <hhheyoni@gmail.com>
2021-04-04 03:21:41 +0900

    (0403) Forbidden 추가

--- pinterest/pragmatic/accountapp/views.py
+++ pinterest/pragmatic/accountapp/views.py
@@ -2,29 +2,53 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse, reverse_lazy
 from accountapp.models import HelloWorld
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
+from .forms import AccountUpdateForm
+
 # Create your views here.
 def hello_world(request):
-    if request.method == "POST":
-        temp = request.POST.get('hello_world_input')
-        new_hello_world = HelloWorld() # models의 새로운 객체가 new~ 에 저장된다.
-        new_hello_world.text = temp # input에서 받은 값을 넣어주기
-        new_hello_world.save() # DB에 저장하기
+    if request.user.is_authenticated:
+        if request.method == "POST":
+            temp = request.POST.get('hello_world_input')
+            new_hello_world = HelloWorld() # models의 새로운 객체가 new~ 에 저장된다.
+            new_hello_world.text = temp # input에서 받은 값을 넣어주기
+            new_hello_world.save() # DB에 저장하기
 
-        # hello_world_list = HelloWorld.objects.all() # object list를 모두 보내줌
-        # return render(request, 'accountapp/hello_world.html', context={'hello_world_list' : hello_world_list}) #이런식으로 사용 시 새로고침하면 기존에 입력했던 값이 계속들어감
-        return HttpResponseRedirect(reverse('accountapp:hello_world')) 
+            # hello_world_list = HelloWorld.objects.all() # object list를 모두 보내줌
+            # return render(request, 'accountapp/hello_world.html', context={'hello_world_list' : hello_world_list}) #이런식으로 사용 시 새로고침하면 기존에 입력했던 값이 계속들어감
+            return HttpResponseRedirect(reverse('accountapp:hello_world')) 
 
+        else:
+            hello_world_list = HelloWorld.objects.all() # object list를 모두 보내줌
+            return render(request, 'accountapp/hello_world.html', context={'hello_world_list':hello_world_list})
     else:
-        hello_world_list = HelloWorld.objects.all() # object list를 모두 보내줌
-        return render(request, 'accountapp/hello_world.html', context={'hello_world_list':hello_world_list})
-
+        return HttpResponseRedirect(reverse('accountapp:login'))
 
 class AccountCreateView(CreateView):
     model = User
     form_class = UserCreationForm
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/create.html'
+
+
+class AccountDetailView(DetailView):
+    model = User
+    context_object_name = 'target_user'
+    template_name = 'accountapp/detail.html'
+
+class AccountUpdateView(UpdateView):
+    model = User
+    context_object_name = 'target_user'
+    form_class = AccountUpdateForm
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'accountapp/update.html'
+
+class AccountDeleteView(DeleteView):
+    model = User
+    context_object_name = 'target_user'
+    success_url = reverse_lazy('accountapp:login') 
+    template_name = 'accountapp/delete.html'
+
