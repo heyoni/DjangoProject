8c04debe1009f465ccb2f0a9de976ca2de5f04a4
heyoni <hhheyoni@gmail.com>
2021-04-04 21:58:50 +0900

    (0403) profile image 업로드 기능

--- pinterest/pragmatic/accountapp/views.py
+++ pinterest/pragmatic/accountapp/views.py
@@ -1,31 +1,38 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect, HttpResponseForbidden
 from django.urls import reverse, reverse_lazy
-from accountapp.models import HelloWorld
+
 from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 
-from .forms import AccountUpdateForm
+from accountapp.models import HelloWorld
+from accountapp.decorators import account_ownership_required
+from accountapp.forms import AccountUpdateForm
 
-# Create your views here.
+
+has_ownership = [account_ownership_required, login_required]
+
+@login_required
+@login_required
 def hello_world(request):
-    if request.user.is_authenticated:
-        if request.method == "POST":
-            temp = request.POST.get('hello_world_input')
-            new_hello_world = HelloWorld() # models의 새로운 객체가 new~ 에 저장된다.
-            new_hello_world.text = temp # input에서 받은 값을 넣어주기
-            new_hello_world.save() # DB에 저장하기
+    if request.method == "POST":
+        temp = request.POST.get('hello_world_input')
+        new_hello_world = HelloWorld() # models의 새로운 객체가 new~ 에 저장된다.
+        new_hello_world.text = temp # input에서 받은 값을 넣어주기
+        new_hello_world.save() # DB에 저장하기
 
-            # hello_world_list = HelloWorld.objects.all() # object list를 모두 보내줌
-            # return render(request, 'accountapp/hello_world.html', context={'hello_world_list' : hello_world_list}) #이런식으로 사용 시 새로고침하면 기존에 입력했던 값이 계속들어감
-            return HttpResponseRedirect(reverse('accountapp:hello_world')) 
+        # hello_world_list = HelloWorld.objects.all() # object list를 모두 보내줌
+        # return render(request, 'accountapp/hello_world.html', context={'hello_world_list' : hello_world_list}) #이런식으로 사용 시 새로고침하면 기존에 입력했던 값이 계속들어감
+        return HttpResponseRedirect(reverse('accountapp:hello_world')) 
 
-        else:
-            hello_world_list = HelloWorld.objects.all() # object list를 모두 보내줌
-            return render(request, 'accountapp/hello_world.html', context={'hello_world_list':hello_world_list})
     else:
-        return HttpResponseRedirect(reverse('accountapp:login'))
+        hello_world_list = HelloWorld.objects.all() # object list를 모두 보내줌
+        return render(request, 'accountapp/hello_world.html', context={'hello_world_list':hello_world_list})
+
+
 
 class AccountCreateView(CreateView):
     model = User
@@ -39,6 +46,8 @@ class AccountDetailView(DetailView):
     context_object_name = 'target_user'
     template_name = 'accountapp/detail.html'
 
+@method_decorator(has_ownership, 'get')
+@method_decorator(has_ownership, 'post')
 class AccountUpdateView(UpdateView):
     model = User
     context_object_name = 'target_user'
@@ -46,32 +55,11 @@ class AccountUpdateView(UpdateView):
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/update.html'
 
-    def get(self, *args, **kwargs):
-        if self.request.user.is_authenticated and self.get_object() == self.request.user:
-            return super().get(*args, **kwargs)
-        else:
-            return HttpResponseForbidden()
 
-    def post(self, *args, **kwargs):
-        if self.request.user.is_authenticated and self.get_object() == self.request.user:
-            return super().post(*args, **kwargs)
-        else:
-            return HttpResponseForbidden()
-
-
+@method_decorator(has_ownership, 'get')
+@method_decorator(has_ownership, 'post')
 class AccountDeleteView(DeleteView):
     model = User
     context_object_name = 'target_user'
     success_url = reverse_lazy('accountapp:login') 
     template_name = 'accountapp/delete.html'
-    def get(self, *args, **kwargs):
-        if self.request.user.is_authenticated and self.get_object() == self.request.user:
-            return super().get(*args, **kwargs)
-        else:
-            return HttpResponseForbidden()
-
-    def post(self, *args, **kwargs):
-        if self.request.user.is_authenticated and self.get_object() == self.request.user:
-            return super().post(*args, **kwargs)
-        else:
-            return HttpResponseForbidden()
