e98305b0b39e650e4757099f588f1f152bab3bc1
heyoni <hhheyoni@gmail.com>
2021-04-12 22:47:40 +0900

    (0412) projectapp 마무리

--- pinterest/pragmatic/accountapp/views.py
+++ pinterest/pragmatic/accountapp/views.py
@@ -7,7 +7,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
-
+from articleapp.models import Article
 from accountapp.models import HelloWorld
 from accountapp.decorators import account_ownership_required
 from accountapp.forms import AccountUpdateForm
@@ -45,6 +45,12 @@ class AccountDetailView(DetailView):
     model = User
     context_object_name = 'target_user'
     template_name = 'accountapp/detail.html'
+    paginate_by = 25
+
+    def get_context_data(self, **kwargs):
+        object_list = Article.objects.filter(writer=self.get_object())
+        return super(AccountDetailView, self).get_context_data(object_list=object_list, **kwargs)
+
 
 @method_decorator(has_ownership, 'get')
 @method_decorator(has_ownership, 'post')
