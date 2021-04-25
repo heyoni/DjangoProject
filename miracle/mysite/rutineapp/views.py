981c2de36d1ecf9cae5e57252c1171a354d15bd7
heyoni <hhheyoni@gmail.com>
2021-04-25 21:43:43 +0900

    (0425) 루틴 create 구현

--- miracle/mysite/rutineapp/views.py
+++ miracle/mysite/rutineapp/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render
 from django.views.generic import CreateView, DetailView, UpdateView, DeleteView, ListView
 from rutineapp.models import Rutine
 from django.core.paginator import Paginator
+from django.contrib.auth.decorators import login_required
 
 from django.utils.decorators import method_decorator
 from django.urls import reverse, reverse_lazy
 
+from rutineapp.forms import RutineCreationForm
 
 # Create your views here.
 class RutineListView(ListView):
@@ -15,7 +17,19 @@ class RutineListView(ListView):
     paginate_by = 25
     queryset = Rutine.objects.all()
 
+# @method_decorator(login_required, 'get')
+# @method_decorator(login_required, 'post')
+class RutineCreateiew(CreateView):
+    model = Rutine
+    form_class = RutineCreationForm
+    template_name = 'create.html'
+    
+    # def form_valid(self, form):
+    #     temp_article = form.save(commit=False)
+    #     temp_article.writer = self.request.user
+    #     temp_article.save()
 
-# class RutineCreateiew(CreateView):
-#     model = Rutine
-#     form_class = 
+    #     return super().form_valid(form)
+
+    # def get_success_url(self):
+    #     return reverse('rutineapp:detail',kwargs={'pk':self.object.pk})
