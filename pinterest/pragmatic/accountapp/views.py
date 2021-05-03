7238bf7e580013b08bccbee390220179beb94c92
heyoni <hhheyoni@gmail.com>
2021-05-03 23:46:49 +0900

    (0503) 에러수정

--- pinterest/pragmatic/accountapp/views.py
+++ pinterest/pragmatic/accountapp/views.py
@@ -41,7 +41,7 @@ class AccountUpdateView(UpdateView):
     model = User
     context_object_name = 'target_user'
     form_class = AccountUpdateForm
-    success_url = reverse_lazy('accountapp:hello_world')
+    success_url = reverse_lazy('accountapp:home')
     template_name = 'accountapp/update.html'
 
 
