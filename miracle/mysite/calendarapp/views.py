97ec64091983ce87542da8ea72e8abb1c8446a51
heyoni <hhheyoni@gmail.com>
2021-05-12 21:09:26 +0900

    (0511) calendar 구현

--- miracle/mysite/calendarapp/views.py
+++ miracle/mysite/calendarapp/views.py
@@ -2,4 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 def CalendarView(request):
+    import calendar
+    c = calendar.TextCalendar(calendar.SUNDAY)
+    s = c.formatmonth(2021,5)
+
+
     return render(request, 'calendarapp/calendar.html')
