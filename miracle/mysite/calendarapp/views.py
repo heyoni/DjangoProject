300bb4f7d87a75d84ac810c8d53c922fac76cfce
heyoni <hhheyoni@gmail.com>
2021-05-15 23:45:21 +0900

    (0515) calendar 엎고 다시

--- miracle/mysite/calendarapp/views.py
+++ miracle/mysite/calendarapp/views.py
@@ -1,16 +1,17 @@
 from django.shortcuts import render
 import calendar
+from calendar import HTMLCalendar
 
 
 # 해야할것: class로 만들 수 있으면 만들고, s를 html단에 넘겨주기
 
 # Create your views here.
-def CalendarView(request):
-    htmlCalendar = calendar.HTMLCalendar(calendar.SUNDAY)
-    s = htmlCalendar.formatmonth(2021,5)
-    s = str(s)
-    return render(request, 'calendarapp/calendar.html', {'s':s})
+def CalendarView(request, year, month):
+    month_number = list(calendar.month_name).index(month)
+    month = month.capitalize()
 
-
-# class CalendarView()
-# 
+    return render(request, 'calendarapp/calendar.html',{
+                                            'year': year,
+                                            'month': month,
+                                            'month_number':month_number,
+                                            })
