1661b5c6c23f9ce803529c255fc64957da580696
heyoni <hhheyoni@gmail.com>
2021-05-28 15:29:58 +0900

    (0528) calendar 미해결

--- miracle/mysite/calendarapp/urls.py
+++ miracle/mysite/calendarapp/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'calendarapp'
 
 urlpatterns = [
-    path('', views.calendar_view, name='calendar'),
+    path('', views.calendarView, name='calendar'),
     path('input/', views.event, name='input'),
     path('edit/<int:event_id>', views.event, name="edit"),
 ]
