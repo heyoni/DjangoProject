ca803c7759fc512f1c493d77f767b720e5e48d8a
heyoni <hhheyoni@gmail.com>
2021-06-22 16:33:38 +0900

    (0622) infoapp 수정

--- miracle/mysite/infoapp/urls.py
+++ miracle/mysite/infoapp/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-
+from infoapp.views import InfoListView
 app_name = 'infoapp'
 
 urlpatterns = [
     # path('update/<int:pk>', ProfileUpdateView.as_view(), name='update'),
-    # path('', ,name='info')
+    path('', InfoListView.as_view() ,name='info')
 ]
--- miracle/mysite/infoapp/views.py
+++ miracle/mysite/infoapp/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from routineapp.models import Routine
+from django.views.generic import ListView
+
 
 # Create your views here.
+class InfoListView(ListView):
+    model = Routine
+    context_object_name = 'routine_info'
+    template_name = 'infoapp/info.html'
+    paginate_by = 25
+    queryset = Routine.objects.all()
