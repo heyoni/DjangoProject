397442b0d519868d0d58b7b2d8d7860f94614246
heyoni <hhheyoni@gmail.com>
2021-07-12 01:21:31 +0900

    (0711) 기본 틀 잡기

---
+++ book/djbook/mysite/bookmark/urls.py
@@ -0,0 +1,9 @@
+# from django.contrib import admin
+# from django.urls import path
+
+# from bookmark.views import BookmarkLV, BookmarkDV
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('bookmark/', admin.site.urls),
+# ]
