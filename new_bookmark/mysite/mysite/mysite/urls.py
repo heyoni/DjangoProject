a159cf14cc4a1c26a5193e48ae43e32373d89c0e
heyoni <hhheyoni@gmail.com>
2021-08-03 23:54:46 +0900

    (0803) login 기능

--- new_bookmark/mysite/mysite/mysite/urls.py
+++ new_bookmark/mysite/mysite/mysite/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from mysite.views import HomeView
+from mysite.views import HomeView, UserCreateView, UserCreateDoneTV
 from django.conf.urls.static import static
 from django.conf import settings
 
@@ -25,7 +25,10 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('bookmark/', include('bookmark.urls')),
     path('blog/', include('blog.urls')),
-    path('photo/', include('photo.urls'))
+    path('photo/', include('photo.urls')),
     
+    path('accounts/', include('django.contrib.auth.urls')), 
+    path('accounts/register/', UserCreateView.as_view(), name='register'), 
+    path('accounts/register/done/', UserCreateDoneTV.as_view(), name='register_done'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
