9b450ac51e17a2e751d33ac3ea729cbbbee3eddd
heyoni <hhheyoni@gmail.com>
2021-10-04 23:04:06 +0900

    (1004) feed list 구현

---
+++ pickyCat/mysite/feedList/admin.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django.contrib import admin
+from .models import FeedList
+ 
+class FeedListModelAdmin(admin.ModelAdmin):
+    list_display = ['name','crude_protein','crude_Fat','phosphorus','liking']
+
+admin.site.register(FeedList, FeedListModelAdmin)
+
+
